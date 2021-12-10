a1ccb6288ddfdc5d34c8e74101e455f387899efa
Markus Bordihn <Markus@Bordihn.de>
2021-12-10 18:28:06 +0100

    Update CriticalVersionEnforcer.java
     Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible.

--- src/main/java/de/markusbordihn/criticalversionenforcer/CriticalVersionEnforcer.java
+++ src/main/java/de/markusbordihn/criticalversionenforcer/CriticalVersionEnforcer.java
@@ -4,8 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod(Constants.MOD_ID)
 public class CriticalVersionEnforcer {
@@ -13,6 +16,12 @@ public class CriticalVersionEnforcer {
   public static final Logger log = LogManager.getLogger(Constants.LOG_NAME);
 
   public CriticalVersionEnforcer() {
+    // Make sure the mod being absent on the other network side does not cause the
+    // client to display the server as incompatible
+    ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+        () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY,
+            (a, b) -> true));
+
     final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
     log.info("Thanks for using critical version enforcer ...");
   }
